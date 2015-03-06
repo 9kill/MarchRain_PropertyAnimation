22ea596efa64c14d3a176de39d3372ba3dbdf03f
sunzhenyu <sunzhenyu@mmbang.net>
2015-03-06 11:53:08 +0800

    add line2

--- src/com/example/propertyanimation/MainActivity.java
+++ src/com/example/propertyanimation/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         sh = getResources().getDisplayMetrics().heightPixels;
         //add line
+        //add line2
     }
 
     /**
